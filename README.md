3d5e24fee0e478079f8cdc7b61d34d1620e9656e
Bharat Rajpurohit <bharatsr100@gmail.com>
2024-10-30 03:53:51 +0530

    Youtube music is opening and existing session is getting used

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Test Environment Setup
 
 1. Install Python
-2. Run the following commands:
+2. Run the following commands to install Selenium and WebDriver Manager:
    ```bash
    pip install selenium --user
    pip install webdriver-manager --user
@@ -11,7 +11,7 @@
 ## Test Environment Setup
 
 1. Create git repository on github
-2. Run the following commands in git cli:
+2. Run the following commands in git cli for initial commit and pushing changes to remote repo:
     ```bash
     git init
     git add .
@@ -19,3 +19,10 @@
     git remote add origin git@github.com:bharatsr100/RPA_Automation.git
     git branch -M main
     git push -u origin main
+
+## Script Instructions
+
+1. Make sure chrome is not opened with user date being used in script. Just close the chrome browser.
+2. Run the script.
+    ```bash
+    python migrate_music.py
--- migrate_music.py
+++ migrate_music.py
@@ -2,14 +2,31 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+# C:\Users\bhara\AppData\Local\Google\Chrome\User Data
+
+# Setup Chrome options
+chrome_options = Options()
+#chrome_options.add_argument('--headless')
+#chrome_options.add_argument("--no-sandbox")
+#chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--user-data-dir=C:/Users/bhara/AppData/Local/Google/Chrome/User Data")
+chrome_options.add_argument("--profile-directory=Default")
+
+
+
 # Set up Chrome WebDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Initialize the WebDriver with options
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
 
 # Open YouTube Music
 driver.get("https://music.youtube.com/")
-time.sleep(20)
+time.sleep(30)
 
 # Optional: Check if the page title contains "YouTube Music" to confirm the page opened correctly
 if "YouTube Music" in driver.title:
